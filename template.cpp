18a97ab6fa7ade1d2ef7ead79203012a505d4366
chachabai <dna049@outlook.com>
2020-09-24 21:41:15 +0800

    update

--- template.cpp
+++ template.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #define watch(x) std::cout << (#x) << " is " << (x) << std::endl
 #define print(x) std::cout << (x) << std::endl
+#define println std::cout << std::endl;
 using LL = long long;
 
 int main() {
