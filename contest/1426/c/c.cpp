d344ee1f20efd875adacdc6b3251a1c621ec530a
chachabai <dna049@outlook.com>
2020-09-29 10:39:50 +0800

    update

--- contest/1426/c/c.cpp
+++ contest/1426/c/c.cpp
@@ -13,10 +13,14 @@ int main() {
 	while (cas--) {
 		int n;
 		std::cin >> n;
-		int r = n - 1;
-		for (int i = 1; i <= n; i = n / (n / i) + 1) {
-			r = std::min(r, i - 2 + (n + i - 1) / i);
-		}
+		// may get a TLE
+		// int r = n - 1;
+		// for (int i = 1; i <= n; i = n / (n / i) + 1) {
+		// 	r = std::min(r, i - 2 + (n + i - 1) / i);
+		// }
+		// 可能取到的最小值
+		int r = 2 * (std::sqrt(n) - 1);
+		while ((r / 2 + 1) * (r - r / 2 + 1) < n) ++r;
 		print(r);
 	}
 	return 0;
